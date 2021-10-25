34caee78162329d825b640884704ad0d424c0908
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-10-25 15:32:21 -0500

    added images to game pieces and game pieces can not pass through the floor of the game board.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const gameboard = document.querySelector('.gameboard');
   let squares = Array.from(document.querySelectorAll('.gameboard div'));
-  width = 10;
+  const colors = [
+    'url(images/blue-tetromino.png)',
+    'url(images/grey-tetromino.png)',
+    'url(images/purple-tetromino.png)',
+    'url(images/green-tetromino.png)',
+    'url(images/pink-tetromino.png)',
+    'url(images/yellow-tetromino.png)',
+    'url(images/red-tetromino.png)',
+  ];
   //Drawing the game pieces
   const lTetromino = [
     [0, 1, 11, 21],
@@ -32,46 +40,80 @@ document.addEventListener('DOMContentLoaded', () => {
   ];
 
   const tTetromino = [
-    [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2 + 1],
-    [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, width * 2 + 1],
+    [1, 10, 11, 12],
+    [1, 11, 12, 21],
+    [10, 11, 12, 21],
+    [1, 10, 11, 21],
   ];
 
   const oTetromino = [
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
+    [0, 1, 10, 11],
+    [0, 1, 10, 11],
+    [0, 1, 10, 11],
+    [0, 1, 10, 11],
   ];
 
   const iTetromino = [
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3],
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3],
+    [1, 11, 21, 31],
+    [10, 11, 12, 13],
+    [1, 11, 21, 31],
+    [10, 11, 12, 13],
   ];
 
   const tetrominoes = [
-    zTetromino,
-    sTetromino,
     lTetromino,
     jTetromino,
+    sTetromino,
+    zTetromino,
     tTetromino,
     oTetromino,
     iTetromino,
   ];
-  let currentPosition = 4;
-  //Picks random tetromino and displays in the first rotation position
+  let currentPosition = 3;
   let currentRotation = 0;
+
+  //Picks random tetromino and displays in the first rotation position
   let random = Math.floor(Math.random() * tetrominoes.length);
   let current = tetrominoes[random][currentRotation];
 
-  function draw() {
+  //Drawing the Tetromino
+  const drawGamePiece = () => {
     current.forEach((index) => {
       squares[currentPosition + index].classList.add('tetromino');
+      squares[currentPosition + index].style.backgroundImage = colors[random];
     });
-  }
+  };
 
-  draw();
+  // Undraw Tetromino
+  const undrawGamePiece = () => {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove('tetromino');
+      squares[currentPosition + index].style.backgroundImage = 'none';
+    });
+  };
+
+  //Stop game pieces from going through the floor
+  const floor = () => {
+    if (
+      current.some((index) => squares[currentPosition + index + 10].classList.contains('floor'))
+    ) {
+      current.forEach((index) => squares[currentPosition + index].classList.add('floor'));
+      //Start a new tetromino falling
+      random = Math.floor(Math.random() * tetrominoes.length);
+      current = Math.floor(Math.random() * tetrominoes.length);
+      current = tetrominoes[random][currentRotation];
+      currentPosition = 4;
+      drawGamePiece();
+    }
+  };
+
+  // Make the game pieces move down every second
+  const down = () => {
+    undrawGamePiece();
+    currentPosition += 10;
+    drawGamePiece();
+    floor();
+  };
+
+  moveTimer = setInterval(down, 1000);
 });
